0a3324b6056966a21711f65dae84c953d5e09a3a
TanBoi <tanguy.boittin@gmail.com>
2020-11-23 20:13:04 +0100

    add project to git

---
+++ js/script1.js
@@ -0,0 +1,9 @@
+Dolmen, Allègre, Nougier, Saint-Just         rang 5
+Equalizer, Botrel, Darbois, Le Gall        rang 4
+Trivial, Allain, Le Fur, Paris            rang 8
+Seek&Share, Boittin, Jagline, Régné         rang 0
+ENIB'labla, Davion, Tertre                rang 2
+Peche-sensible, Bacher, More            rang 1
+Fridgelibre, Jones, Marchese            rang 7
+Know'ceans, Brosseau, Pochard            rang 6
+ExoShare, Abjean, Brisson, Le Sech, Mouellic    rang 3
